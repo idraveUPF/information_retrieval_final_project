8628643c0270fcc6d614deb4a9cc5fb7d85fc63e
idraveUPF@git.com <ivandaniel.rodriguez01@estudiant.upf.edu>
2020-11-21 18:03:33 +0100

    Updated tweet_stream

--- search-engine/tweet_stream.py
+++ search-engine/tweet_stream.py
@@ -3,24 +3,17 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 
-consumer_key = 'UPtQiALNjB7rWxcIY8CB9IXmb'
-consumer_secret = 'lsU5NELQCDfE7eGs0f2fraobK34LoF7X0OSreaOAdLR7n7uwv8'
-access_token = '3796381515-0VrTXAZlasSMTWGlYBDa5djTsKjS6hl1eqrHmIt'
-access_secret = 'juiVqZVBAoqQyFjXfsWWt8LkBM0ikqpNsWmhHrh8ZyHA3'
- 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
- 
-
 class MyListener(StreamListener):
     
-    def __init__(self, api=None):
+    def __init__(self, max_tweets, output_file='Output.json'):
         super(StreamListener, self).__init__()
         self.num_tweets = 0
+        self.max_tweets = max_tweets
+        self.output_file = output_file
 
     def on_data(self, data):
         try:
-            with open('Output.json', 'w') as f:
+            with open(self.output_file, 'a') as f:
                 jdata = json.loads(data)
                 filter_data = {}
                 if 'extended_tweet' in jdata: 
@@ -49,7 +42,7 @@ class MyListener(StreamListener):
                 f.write(json.dumps(filter_data))
 
                 # Setting a limit in the number of tweets collected
-                if self.num_tweets < 1: 
+                if self.num_tweets < self.max_tweets:
                     return True
                 else:
                     return False
@@ -57,13 +50,22 @@ class MyListener(StreamListener):
         except BaseException as e:
             print("Error on_data: %s" % str(e))
         return True
-
  
     def on_error(self, status):
         print('Error :', status.place)
         return False
-    
-twitter_stream = Stream(auth, MyListener())
-twitter_stream.filter(track=["BlackLivesMatter", "AllLivesMatter", "BlueLivesMatter", "BLM", "blackpower", "blackpride", "africanamerican", "racism", "blackhistory", "equality", "policebrutality"], languages=['en']) # Add your keywords and other filters
 
-print('_______ End _______')
+KEYWORDS = ["BlackLivesMatter", "AllLivesMatter", "BlueLivesMatter", "BLM", "blackpower", "blackpride", "africanamerican", "racism", "blackhistory", "equality", "policebrutality"]
+
+if __name__ == '__main__':
+    consumer_key = 'UPtQiALNjB7rWxcIY8CB9IXmb'
+    consumer_secret = 'lsU5NELQCDfE7eGs0f2fraobK34LoF7X0OSreaOAdLR7n7uwv8'
+    access_token = '3796381515-0VrTXAZlasSMTWGlYBDa5djTsKjS6hl1eqrHmIt'
+    access_secret = 'juiVqZVBAoqQyFjXfsWWt8LkBM0ikqpNsWmhHrh8ZyHA3'
+
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    twitter_stream = Stream(auth, MyListener())
+    twitter_stream.filter(track=KEYWORDS, languages=['en']) # Add your keywords and other filters
+
+    print('_______ End _______')
